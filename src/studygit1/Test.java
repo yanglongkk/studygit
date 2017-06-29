cf6da3131f879f6a5d810fb602e92f46025857b2
yanglong <735778875@qq.com>
2017-06-29 15:15:04 +0800

    创建项目

---
+++ src/studygit1/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package studygit1;
+
+/**
+ * @author Dellice
+ * 2017Äê6ÔÂ29ÈÕ
+ */
+public class Test {
+
+	/**
+	 *@author Dellice
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gitstudy");
+	}
+
+}
